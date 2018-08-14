83bb4dcda686ea27ed40cccb8deda47aeb0ec605
Joshua Robbins <jrobbin@sandia.gov>
2018-08-14 08:59:22 -0600

    added build defaults

---
+++ base/config/defaults.cmake
@@ -0,0 +1,8 @@
+option( PLATOMAIN     "Flag to turn on compilation of PlatoMain"           OFF )
+option( PLATOSTATICS  "Flag to turn on compilation of Statics performer"   OFF )
+option( SALINAS       "Flag to turn on testing of Salinas performer"       OFF )
+option( ALBANY        "Flag to turn on testing of Albany performer"        OFF )
+option( ANALYZE       "Flag to turn on testing of Plato Analyze performer" OFF )
+option( REGRESSION    "Flag to create regression test suite"               OFF )
+option( UNIT_TESTING  "Flag to turn on unit testing"                       OFF )
+option( ENABLE_ISO    "Flag to turn on iso extraction"                     OFF )
