a0efc7d7f6cf815912e88ce0b0e0b526c3557d40
Miguel A. Aguilo <maguilo@sandia.gov>
2018-10-02 10:17:15 -0600

    plato: formatting

--- base/src/PlatoSubproblemLibrary/DiscreteGlobalOptimization/PSL_DiscreteObjective.hpp
+++ base/src/PlatoSubproblemLibrary/DiscreteGlobalOptimization/PSL_DiscreteObjective.hpp
@@ -15,10 +15,10 @@ public:
     DiscreteObjective();
     virtual ~DiscreteObjective();
 
-    virtual void get_domain(std::vector<double>& inclusive_lower,
-                            std::vector<double>& inclusive_upper,
-                            std::vector<int>& num_values) = 0;
-    virtual double evaluate(const std::vector<double>& parameters) = 0;
+    virtual void get_domain(std::vector<double>& aInclusiveLower,
+                            std::vector<double>& aInclusiveUpper,
+                            std::vector<int>& aNumValues) = 0;
+    virtual double evaluate(const std::vector<double>& aParameters) = 0;
 
 protected:
 
