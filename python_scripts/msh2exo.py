1f029858894ad525f501a7f94d465cafb5090e6f
Miguel A. Aguilo <maguilo@sandia.gov>
2020-01-03 11:05:45 -0700

    now accepting filename input with or without .msh extension

--- python_scripts/msh2exo.py
+++ python_scripts/msh2exo.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 import os, sys
 
-tPath = str(sys.argv[1])
-if tPath[-1:] == "/":
-  tmhs2osh_root = tPath + "src/msh2osh "
-  tosh2exo_root = tPath + "src/osh2exo "
+tPathInput = str(sys.argv[1])
+if tPathInput[-1:] == "/":
+  tmhs2osh_root = tPathInput + "src/msh2osh "
+  tosh2exo_root = tPathInput + "src/osh2exo "
 else:
-  tmhs2osh_root = tPath + "/src/msh2osh "
-  tosh2exo_root = tPath + "/src/osh2exo "
+  tmhs2osh_root = tPathInput + "/src/msh2osh "
+  tosh2exo_root = tPathInput + "/src/osh2exo "
 
-tmhs2osh_command = tmhs2osh_root + str(sys.argv[2]) + ".msh " + str(sys.argv[2]) + ".osh" 
+tNameInput = str(sys.argv[2])
+if tNameInput[-4:] == ".msh":
+  tName = tNameInput[:-4]
+else:
+  tName = tNameInput
+
+tmhs2osh_command = tmhs2osh_root + tName + ".msh " + tName + ".osh" 
 tSuccess = os.system(tmhs2osh_command)
-print('msh2osh command was executed successfuly:', tSuccess)
+print('msh2osh command was successful:', tSuccess)
 
-tosh2exo_command = tosh2exo_root + str(sys.argv[2]) + ".osh " + str(sys.argv[2]) + ".exo"
+tosh2exo_command = tosh2exo_root + tName + ".osh " + tName + ".exo"
 tSuccess = os.system(tosh2exo_command)
-print('osh2exo command executed successfuly:', tSuccess)
+print('osh2exo command was successful:', tSuccess)
