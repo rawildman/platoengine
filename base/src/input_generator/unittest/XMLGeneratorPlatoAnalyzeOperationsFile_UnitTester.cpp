7713a171d1910e20ae253ab4eca698aea9bffd40
maguilo11 <aguilo.miguel@gmail.com>
2020-06-02 13:55:43 -0600

    removed dependency and added source file for new set of unit tests

---
+++ base/src/input_generator/unittest/XMLGeneratorPlatoAnalyzeOperationsFile_UnitTester.cpp
@@ -0,0 +1,20 @@
+/*
+ * XMLGeneratorPlatoAnalyzeOperationsFile_UnitTester.cpp
+ *
+ *  Created on: Jun 2, 2020
+ */
+
+#include <gtest/gtest.h>
+
+#include "XMLGenerator_UnitTester_Tools.hpp"
+
+namespace XMLGen
+{
+
+}
+
+namespace PlatoTestXMLGenerator
+{
+
+}
+// namespace PlatoTestXMLGenerator
