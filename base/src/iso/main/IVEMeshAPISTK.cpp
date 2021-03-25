a47830df8dc4fcd06d42e3e1c6f3eda9bc0fa2fb
Brett Clark <bwclark@sandia.gov>
2021-03-25 09:34:12 -0600

    Backporting a fix by Alan Williams that was done in the sierra PE repo so that it gets back into the main PE repo on github and is not lost.

--- base/src/iso/main/IVEMeshAPISTK.cpp
+++ base/src/iso/main/IVEMeshAPISTK.cpp
@@ -1058,43 +1058,39 @@ void IVEMeshAPISTK::prepare_field_data(std::string &outputFieldsString, IVEMeshA
       outAPI->prepare_as_destination();
 }
 
+void convert_block_names_to_parts(const stk::mesh::MetaData& meta,
+                                  const std::vector<std::string>& blocks,
+                                  stk::mesh::PartVector& blockParts)
+{
+  blockParts.clear();
+  for(const std::string& block : blocks)
+  {
+    std::string blockName = "block_" + block;
+    stk::mesh::Part* partPtr = meta.get_part(blockName);
+    if (partPtr != nullptr)
+    { 
+      blockParts.push_back(partPtr);
+    }
+    else
+      throw std::runtime_error("Could not find STK part for the block named: " + blockName);
+  }
+}
+
 void IVEMeshAPISTK::get_fixed_block_nodes(std::vector<IVEHandle> &fixed_block_nodes)
 {
   fixed_block_nodes.clear();
   if(mFixedBlocks.size() > 0)
   {
-    std::string part_name = "block_" + mFixedBlocks[0];
-    stk::mesh::Part* tCurPart = mMetaData->get_part(part_name);
-    if(tCurPart != nullptr)
-    { 
-      stk::mesh::Selector sel(*tCurPart);
-      for(size_t i=1; i<mFixedBlocks.size(); ++i)
+    stk::mesh::PartVector blockParts;
+    convert_block_names_to_parts(*mMetaData, mFixedBlocks, blockParts);
+    stk::mesh::Selector sel = stk::mesh::selectUnion(blockParts);
+    const stk::mesh::BucketVector& fixed_node_buckets = mBulkData->get_buckets(stk::topology::NODE_RANK, sel);
+    for(const stk::mesh::Bucket* bucket : fixed_node_buckets)
+    {
+      for(stk::mesh::Entity node : *bucket)
       {
-        part_name = "block_" + mFixedBlocks[i];
-        tCurPart = mMetaData->get_part(part_name);
-        if(tCurPart != nullptr)
-        {
-          sel |= *tCurPart;
-        }
-        else
-        {      
-          throw std::runtime_error("Could not find STK part corresponding to name: " + part_name);
-        }
+        fixed_block_nodes.push_back(get_handle(node));
       }
-      stk::mesh::BucketVector fixed_node_buckets;
-      fixed_node_buckets = mBulkData->get_buckets(stk::topology::NODE_RANK, sel);
-      for(size_t i=0; i<fixed_node_buckets.size(); ++i) 
-      {
-        stk::mesh::Bucket &cur_bucket = *fixed_node_buckets[i];
-        for(size_t j=0; j<cur_bucket.size(); ++j)
-        {
-          fixed_block_nodes.push_back(get_handle(cur_bucket[j]));
-        }
-      }
-    }
-    else
-    {      
-      throw std::runtime_error("Could not find STK part corresponding to name: " + part_name);
     }
   }
   // uniquify the list
