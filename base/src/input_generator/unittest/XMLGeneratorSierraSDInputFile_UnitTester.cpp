6cf54222a733081f7c0d549b8ca5667dcaa4e19f
Sean Hardesty <shardes@sandia.gov>
2021-06-29 10:44:18 -0600

    add unit test with multiple cases and sections

--- base/src/input_generator/unittest/XMLGeneratorSierraSDInputFile_UnitTester.cpp
+++ base/src/input_generator/unittest/XMLGeneratorSierraSDInputFile_UnitTester.cpp
@@ -147,4 +147,62 @@ TEST(PlatoTestXMLGenerator, SD_augmentEchoSection)
     EXPECT_EQ(strcmp(oDeck.str().c_str(),expected_output),0);
 }
 
+TEST(PlatoTestXMLGenerator, SD_deckWithManySections)
+{
+    constexpr char input[] =
+    "SOLUTION\n"
+    "  case '1'\n"
+    "  statics\n"
+    "  case '2'\n"
+    "  statics\n"
+    "  solver gdsw\n"
+    "END\n"
+    "MATERIAL 1\n"
+    "  isotropic\n"
+    "  E = 1e9\n"
+    "  nu = .33\n"
+    "END\n"
+    "OUTPUTS\n"
+    "  disp\n"
+    "END\n"
+    "ECHO\n"
+    "  disp\n"
+    "END\n";
+
+    std::stringstream iDeck(input), oDeck;
+
+    XMLGen::InputData aXMLMetaData = setUpMetaDataForExistingSDInputDeck(input);
+    XMLGen::augment_sierra_sd_input_deck_with_plato_problem_description(aXMLMetaData, iDeck, oDeck);
+
+    constexpr char expected_output[] =
+    "SOLUTION\n"
+    "  case '1'\n"
+    "  topology_optimization\n"
+    "  case '2'\n"
+    "  topology_optimization\n"
+    "  solver gdsw\n"
+    "END\n"
+    "MATERIAL 1\n"
+    "  isotropic\n"
+    "  E = 1e9\n"
+    "  nu = .33\n"
+    "  material_penalty_model = simp\n"
+    "END\n"
+    "OUTPUTS\n"
+    "  disp\n"
+    "  topology\n"
+    "END\n"
+    "ECHO\n"
+    "  disp\n"
+    "  topology\n"
+    "END\n"
+    "TOPOLOGY-OPTIMIZATION\n"
+    "  algorithm = plato_engine\n"
+    "  volume_fraction = .314\n"
+    "END\n";
+
+    EXPECT_EQ(strcmp(oDeck.str().c_str(),expected_output),0);
+}
+
+
 } // namespace PlatoTestXMLGenerator
