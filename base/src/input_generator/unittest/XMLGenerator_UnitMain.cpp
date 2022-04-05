9024a5a92cc3e36931bfccf64003e4cf635ab507
ralberd <ralberd@sandia.gov>
2022-04-05 14:21:49 -0600

    getting rid of unnecessary kokkos initialization and finalization

--- base/src/input_generator/unittest/XMLGenerator_UnitMain.cpp
+++ base/src/input_generator/unittest/XMLGenerator_UnitMain.cpp
@@ -48,13 +48,9 @@ int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
-    Kokkos::initialize(argc, argv);
-
     testing::InitGoogleTest(&argc, argv);
     int returnVal = RUN_ALL_TESTS();
 
-    Kokkos::finalize();
-    
     MPI_Finalize();
 
     return returnVal;
