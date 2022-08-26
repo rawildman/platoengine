f9165c8229161e628884485f062ea8771e4f5f97
Ryan Viertel <rvierte@sandia.gov>
2022-08-26 16:36:59 -0600

    reverts PE to LayoutRight

--- base/src/optimize/Plato_KokkosTypes.hpp
+++ base/src/optimize/Plato_KokkosTypes.hpp
@@ -58,7 +58,8 @@ namespace Plato
 using Scalar = double;
 using ExecSpace = Kokkos::DefaultExecutionSpace;
 using MemSpace = typename ExecSpace::memory_space;
-using Layout = typename ExecSpace::array_layout;
+//using Layout = typename ExecSpace::array_layout;
+using Layout = typename Kokkos::LayoutRight;
 
 template <typename ScalarType>
 using ScalarVectorT = typename Kokkos::View<ScalarType*, MemSpace>;
