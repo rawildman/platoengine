4863f7add8b3c047c07d1e2a032e176a75954bf5
Joshua Robbins <jrobbin@sandia.gov>
2022-08-26 17:10:22 -0600

    changed Layout

--- base/src/optimize/Plato_KokkosTypes.hpp
+++ base/src/optimize/Plato_KokkosTypes.hpp
@@ -58,7 +58,7 @@ namespace Plato
 using Scalar = double;
 using ExecSpace = Kokkos::DefaultExecutionSpace;
 using MemSpace = typename ExecSpace::memory_space;
-using Layout = typename ExecSpace::array_layout;
+using Layout = Kokkos::LayoutRight;
 
 template <typename ScalarType>
 using ScalarVectorT = typename Kokkos::View<ScalarType*, MemSpace>;
