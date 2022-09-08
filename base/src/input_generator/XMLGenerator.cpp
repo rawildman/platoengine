4cbb6f36b34b4dda8c18a336ad746ec1ffe115cf
Ray Wildman <rawildm@sandia.gov>
2022-09-08 14:00:23 -0600

    Adds an error for using shape opt with ROL LC
    TO-5070

--- base/src/input_generator/XMLGenerator.cpp
+++ base/src/input_generator/XMLGenerator.cpp
@@ -1,52 +1,3 @@
-/*
-//@HEADER
-// *************************************************************************
-//   Plato Engine v.1.0: Copyright 2018, National Technology & Engineering
-//                    Solutions of Sandia, LLC (NTESS).
-//
-// Under the terms of Contract DE-NA0003525 with NTESS,
-// the U.S. Government retains certain rights in this software.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// 1. Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the Sandia Corporation nor the names of the
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Questions? Contact the Plato team (plato3D-help@sandia.gov)
-//
-// *************************************************************************
-//@HEADER
-*/
-
-/*
- * XMLGenerator.cpp
- *
- *  Created on: Sep 28, 2017
- *
- */
-
 #include <set>
 #include <string>
 #include <iostream>
@@ -108,12 +59,6 @@ namespace XMLGen
         m_InputData.m_Arch = arch;
     }
 
-    /******************************************************************************/
-    XMLGenerator::~XMLGenerator()
-    /******************************************************************************/
-    {
-    }
-
     // /******************************************************************************//**
     //  * \fn writeInputFiles
     //  * \brief Write input files, i.e. write all the XML files needed by Plato.
@@ -148,17 +93,22 @@ namespace XMLGen
     {
         if (aInputData.optimization_parameters().optimizationType() == OT_SHAPE)
         {
-            std::string tNumDesignVariablesString = aInputData.optimization_parameters().num_shape_design_variables();
-            int tNumDesignVariables = std::atoi(tNumDesignVariablesString.c_str());
+            const std::string tNumDesignVariablesString = aInputData.optimization_parameters().num_shape_design_variables();
+            const int tNumDesignVariables = std::atoi(tNumDesignVariablesString.c_str());
             for (auto tShapeServiceID : aInputData.objective.shapeServiceIDs)
             {
-                XMLGen::Service tCurService = aInputData.service(tShapeServiceID);
-                std::string tNumProcsString = tCurService.numberProcessors();
-                int tNumProcs = std::atoi(tNumProcsString.c_str());
+                const XMLGen::Service& tCurService = aInputData.service(tShapeServiceID);
+                const std::string tNumProcsString = tCurService.numberProcessors();
+                const int tNumProcs = std::atoi(tNumProcsString.c_str());
                 if (tNumProcs > 1 && tNumProcs != tNumDesignVariables)
                 {
                     THROWERR("If using more than one processor in the service that is calculating the shape sensitivities you must use the same number of processors as num_shape_design_variables specified in the optimization_parameters block.");
                 }
+            }
+            // Check optimization algorithm, ROL LC does not work with shape optimization
+            if (aInputData.optimization_parameters().optimization_algorithm() == "rol_linear_constraint")
+            {
+                THROWERR("The ROL LC algorithm does not work with shape optimization. Choose another optimization algorithm.");
             }
         }
     }
