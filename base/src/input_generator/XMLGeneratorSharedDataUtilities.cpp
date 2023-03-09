0ab9453007f4f0cb2084c3a6c393b60d48a36758
Joshua Robbins <jrobbin@sandia.gov>
2023-03-09 09:34:38 -0700

    removed commented code.  changed default esp workflow to afl4_aflr3.

--- base/src/input_generator/XMLGeneratorSharedDataUtilities.cpp
+++ base/src/input_generator/XMLGeneratorSharedDataUtilities.cpp
@@ -302,16 +302,6 @@ void append_gradient_based_criterion_shared_data
         }
         else if(aMetaData.optimization_parameters().optimizationType() == OT_SHAPE)
         {
-//            if(aMetaData.optimization_parameters().esp_workflow() == "aflr4_aflr3")
-//            {
-//                tTag = std::string("Criterion Gradient - ") + tIdentifierString;
-//                tValues = { tTag, "Scalar", "Global", aMetaData.optimization_parameters().num_shape_design_variables(), tOwnerName, tFirstPlatoMainPerformer };
-//                tSharedDataNode = aDocument.append_child("SharedData");
-//                XMLGen::append_children(tKeys, tValues, tSharedDataNode);
-//            }
-//            else if(aMetaData.optimization_parameters().esp_workflow() == "egads_tetgen" ||
-//                    aMetaData.optimization_parameters().esp_workflow() == "aflr4_tetgen")
-//            {
                 tKeys = { "Name", "Type", "Layout", "Dynamic", "OwnerName", "UserName" };
                 tTag = std::string("Criterion GradientX - ") + tIdentifierString;
                 tValues = { tTag, "Scalar", "Global", "true", tOwnerName, tFirstPlatoMainPerformer };
@@ -324,11 +314,6 @@ void append_gradient_based_criterion_shared_data
                             tFirstPlatoMainPerformer };
                 tSharedDataNode = aDocument.append_child("SharedData");
                 XMLGen::append_children(tKeys, tValues, tSharedDataNode);
-//            }
-//            else
-//            {
-//                THROWERR("Unrecognized type of esp workflow in append_gradient_based_criterion_shared_data().")
-//            }
         }
     }
 }
