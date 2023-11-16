778a10118201305e3ad32673a2f31a911a75a0d4
Ray Wildman <rawildm@sandia.gov>
2023-11-16 09:05:17 -0700

    Moves geometry registration utilities and adds tests
    TO-6862

--- base/src/functional/geometry/GeometryFactory.cpp
+++ base/src/functional/geometry/GeometryFactory.cpp
@@ -1,87 +1,20 @@
 #include "GeometryFactory.hpp"
 
-#include <optional>
-
 #include "BrickShapeGeometry.hpp"
 #include "DensityTopology.hpp"
 #include "Exception.hpp"
 #include "GeometryRegistration.hpp"
 #include "Plato_InputBlocks.hpp"
+#include "detail/GeometryRegistrationUtilities.hpp"
 
 namespace Plato::Functional::GeometryFactory
 {
-
-namespace
-{
-
-template <typename T, typename VARIANT_T>
-struct isVariantMember;
-template <typename T, typename... ALL_T>
-struct isVariantMember<T, std::variant<ALL_T...>> : public std::disjunction<std::is_same<T, ALL_T>...>
-{
-};
-
-template <typename T>
-[[nodiscard]] std::optional<GeometryFactory::GeometryInput> make_variant(const T&)
-{
-    return std::nullopt;
-}
-
-template <typename T>
-[[nodiscard]] std::optional<GeometryFactory::GeometryInput> make_variant(const boost::optional<T>& aObj)
-{
-    if (isVariantMember<T, GeometryFactory::GeometryInput>::value && aObj)
-    {
-        return std::make_optional(GeometryFactory::GeometryInput{aObj.value()});
-    }
-    return std::nullopt;
-}
-
-template <std::size_t... Is>
-[[nodiscard]] std::optional<GeometryFactory::GeometryInput> geometry_input_impl(
-    const Plato::PlatoInput& aInput, std::integer_sequence<std::size_t, Is...>)
-{
-    std::optional<GeometryFactory::GeometryInput> tGeometryInput;
-    ((tGeometryInput = make_variant(boost::fusion::at_c<Is>(aInput))) || ...);
-    return tGeometryInput;
-}
-
-[[nodiscard]] std::optional<GeometryFactory::GeometryInput> geometry_block(const Plato::PlatoInput& aInput)
-{
-    constexpr auto tNumInputFields = boost::fusion::result_of::size<Plato::PlatoInput>::value;
-    return geometry_input_impl(aInput, std::make_index_sequence<tNumInputFields>{});
-}
-
-[[nodiscard]] GeometryFactory::GeometryInput geometry_input(const Plato::PlatoInput& aInput)
-{
-    std::optional<GeometryFactory::GeometryInput> tGeometryInput = geometry_block(aInput);
-    if (!tGeometryInput)
-    {
-        throw Exception("No geometry block was defined.");
-    }
-    return tGeometryInput.value();
-}
-
-[[nodiscard]] std::string block_name(const GeometryFactory::GeometryInput& aInput)
-{
-    return std::visit(
-        [](const auto& aObj) -> std::string
-        {
-            using InputType = std::decay_t<decltype(aObj)>;
-            return Plato::block_name<InputType>();
-        },
-        aInput);
-    throw Exception("No geometry block was defined.");
-}
-
-}  // namespace
-
 GeometryFunction make_geometry_function(const Plato::PlatoInput& aInput)
 {
-    const GeometryInput tGeometryInput = geometry_input(aInput);
+    const GeometryInput tGeometryInput = Detail::geometry_input(aInput);
 
     if (const auto tIter =
-            detail::registered_functions<GeometryFunction, GeometryInput>().find(block_name(tGeometryInput));
+            detail::registered_functions<GeometryFunction, GeometryInput>().find(Detail::block_name(tGeometryInput));
         tIter != detail::registered_functions<GeometryFunction, GeometryInput>().end())
     {
         return tIter->second(tGeometryInput);
---
+++ base/src/functional/geometry/unittest/Test_GeometryRegistrationUtilities.cpp
@@ -0,0 +1,90 @@
+#include <gtest/gtest.h>
+
+#include "detail/GeometryRegistrationUtilities.hpp"
+
+TEST(GeometryRegistrationUtilities, IsVariant)
+{
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+
+    using TestVariant = std::variant<double, char, bool>;
+    constexpr bool tDoubleInVariant = pfgd::IsVariantMember<double, TestVariant>::value;
+    EXPECT_TRUE(tDoubleInVariant);
+
+    constexpr bool tIntInVariant = pfgd::IsVariantMember<int, TestVariant>::value;
+    EXPECT_FALSE(tIntInVariant);
+}
+
+TEST(GeometryRegistrationUtilities, MakeVariantWrongType)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    const std::optional<pfg::GeometryInput> tResult = pfgd::make_variant_if_geometry(42);
+    EXPECT_FALSE(tResult.has_value());
+}
+
+TEST(GeometryRegistrationUtilities, MakeVariantRightTypeEmpty)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    using ValidOptional = boost::optional<Plato::density_topology>;
+    const std::optional<pfg::GeometryInput> tResult = pfgd::make_variant_if_geometry(ValidOptional{boost::none});
+    EXPECT_FALSE(tResult.has_value());
+}
+
+TEST(GeometryRegistrationUtilities, MakeVariantRightTypeNotEmpty)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    using ValidOptional = boost::optional<Plato::density_topology>;
+    const std::optional<pfg::GeometryInput> tResult =
+        pfgd::make_variant_if_geometry(ValidOptional{Plato::density_topology{}});
+    EXPECT_TRUE(tResult.has_value());
+}
+
+TEST(GeometryRegistrationUtilities, GeometryBlockAllEmpty)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    const std::optional<pfg::GeometryInput> tGeometryInput = pfgd::geometry_block(Plato::PlatoInput{});
+    EXPECT_FALSE(tGeometryInput.has_value());
+}
+
+TEST(GeometryRegistrationUtilities, GeometryBlockHasDensityTopology)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    auto tInput = Plato::PlatoInput{};
+    tInput.mDensityTopology.emplace();
+    const std::optional<pfg::GeometryInput> tGeometryInput = pfgd::geometry_block(tInput);
+    EXPECT_TRUE(tGeometryInput.has_value());
+}
+
+TEST(GeometryRegistrationUtilities, GeometryInputAllEmpty)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    EXPECT_THROW(auto tGeometryInput = pfgd::geometry_input(Plato::PlatoInput{}), Plato::Functional::Exception);
+}
+
+TEST(GeometryRegistrationUtilities, GeometryInputHasDensityTopology)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+    namespace pfgd = Plato::Functional::GeometryFactory::Detail;
+    auto tInput = Plato::PlatoInput{};
+    tInput.mDensityTopology.emplace();
+    EXPECT_NO_THROW(auto tGeometryInput = pfgd::geometry_block(tInput));
+}
+
+TEST(GeometryRegistrationUtilities, BlockName)
+{
+    namespace pfg = Plato::Functional::GeometryFactory;
+
+    {
+        const auto tGeometryInput = pfg::GeometryInput{Plato::density_topology{}};
+        EXPECT_EQ(pfg::Detail::block_name(tGeometryInput), "density_topology");
+    }  // namespace Plato::Functional::GeometryFactory;
+    {
+        const auto tGeometryInput = pfg::GeometryInput{Plato::brick_shape_geometry{}};
+        EXPECT_EQ(pfg::Detail::block_name(tGeometryInput), "brick_shape_geometry");
+    }
+}
