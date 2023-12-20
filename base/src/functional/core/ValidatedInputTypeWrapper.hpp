d825c8a1616529f043c3c78e6047b27e3c09b6b8
Adam Sokolow <acsokol@sandia.gov>
2023-12-20 12:54:19 -0700

    Adds validated input type wrapper that is a friend of the validated input class. Only this wrapper can be returned from a validated plato input [ci skip]

---
+++ base/src/functional/core/ValidatedInputTypeWrapper.hpp
@@ -0,0 +1,30 @@
+#ifndef PLATO_FUNCTIONAL_VALIDATEDINPUTTYPEWRAPPER
+#define PLATO_FUNCTIONAL_VALIDATEDINPUTTYPEWRAPPER
+
+#include <utility>
+
+namespace Plato::Functional::Validation
+{
+class ValidatedInput;
+}
+
+namespace Plato::Functional::Core
+{
+
+template <typename InputType>
+class ValidatedInputTypeWrapper
+{
+   public:
+    const InputType& value() const
+    {
+        return mValue;
+    }
+
+   private:
+    friend class Validation::ValidatedInput;
+    ValidatedInputTypeWrapper(InputType aValue) : mValue(std::move(aValue)) {}
+    InputType mValue;
+};
+}  // namespace Plato::Functional::Core
+
+#endif
