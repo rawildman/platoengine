dd983f494da86053216dded817e88ff3bd2c5796
Ray Wildman <rawildm@sandia.gov>
2024-02-02 08:18:27 -0700

    Adds cmake functions to top-level functional directory
    TO-6953

---
+++ base/src/functional/cmake/plato_functional_setup.cmake
@@ -0,0 +1,5 @@
+macro(plato_functional_setup)
+    set(TEST_UNIT_MAIN_INCL ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TestMain.cpp)
+    set(PARALLEL_TEST_UNIT_MAIN_INCL ${CMAKE_CURRENT_SOURCE_DIR}/cmake/ParallelUnitMain.cpp)
+    set(CMAKE_UTIL_DIR ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+endmacro(plato_functional_setup)
