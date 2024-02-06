674ee4b813994591cf573a2bbcc80d9825748459
Ray Wildman <rawildm@sandia.gov>
2024-02-06 13:39:23 -0700

    Simplifies cmake utility for creating unit testers
    TO-6953

--- base/src/functional/cmake/create_plato_unittester.cmake
+++ base/src/functional/cmake/create_plato_unittester.cmake
@@ -1,29 +1,53 @@
 include(${CMAKE_UTIL_DIR}/add_to_srcs_and_hdrs.cmake)
 
 # create_plato_unittester
-#   TEST_EXE: The name of the test executable and CMake target that will be created
+#   TEST_LIB: The name of the library to create a unit tester for. The test exe name will be
+#    the name of the library with `_UnitTester` appended.
 #   DIRECTORIES: A list of directories containing the test source files.
-#   TARGET_LINK_LIST: A list of targets to link against.
-function( create_plato_unittester TEST_EXE DIRECTORIES TARGET_LINK_LIST)
+function( create_plato_unittester TEST_LIB DIRECTORIES)
 
+    set(TEST_EXE "${TEST_LIB}_UnitTester")
+    set(TARGET_LINK_LIST "${TEST_LIB}")
     create_plato_unittester_impl( ${TEST_EXE} "${DIRECTORIES}" ${TEST_UNIT_MAIN_INCL} "${TARGET_LINK_LIST}" )
 
 endfunction(create_plato_unittester)
 
-# create_plato_parallel_unittester
-#   TEST_EXE: The name of the test executable and CMake target that will be created
+# create_plato_integration_tester
+#   TEST_EXE: The name of the test executable and CMake target.
 #   DIRECTORIES: A list of directories containing the test source files.
-#   TARGET_LINK_LIST: A list of targets to link against.
-#   NUM_RANKS_FOR_TEST: The number of MPI ranks to use in the test executable.
-function( create_plato_parallel_unittester TEST_EXE DIRECTORIES TARGET_LINK_LIST NUM_RANKS_FOR_TEST)
+#   TARGET_LINK_LIST: List of targets to link with.
+function( create_plato_integration_tester TEST_EXE DIRECTORIES TARGET_LINK_LIST)
 
-    # Configure NUM_RANKS_FOR_TEST
+    create_plato_unittester_impl( ${TEST_EXE} "${DIRECTORIES}" ${TEST_UNIT_MAIN_INCL} "${TARGET_LINK_LIST}" )
+
+endfunction(create_plato_integration_tester)
+
+# create_plato_parallel_unittester
+#   TEST_LIB: The name of the library to create a unit tester for. The test exe name will be
+#    the name of the library with `_ParallelUnitTester` appended.
+#   DIRECTORIES: A list of directories containing the test source files.
+#   NUM_RANKS_FOR_TEST: The number of MPI ranks to use in the test executable.
+function( create_plato_parallel_unittester TEST_LIB DIRECTORIES NUM_RANKS_FOR_TEST)
+
+    set(TEST_EXE "${TEST_LIB}_ParallelUnitTester")
+    set(TARGET_LINK_LIST "${TEST_LIB}")
+    create_plato_parallel_integration_tester( ${TEST_EXE} "${DIRECTORIES}" "${TARGET_LINK_LIST}" ${NUM_RANKS_FOR_TEST})
+
+endfunction(create_plato_parallel_unittester)
+
+# create_plato_parallel_integration_tester
+#   TEST_EXE: The name of the test executable and CMake target.
+#   DIRECTORIES: A list of directories containing the test source files.
+#   TARGET_LINK_LIST: List of targets to link with.
+#   NUM_RANKS_FOR_TEST: The number of MPI ranks to use in the test executable.
+function( create_plato_parallel_integration_tester TEST_EXE DIRECTORIES TARGET_LINK_LIST NUM_RANKS_FOR_TEST)
+
     configure_file(${PARALLEL_TEST_UNIT_MAIN_INCL} ${CMAKE_CURRENT_BINARY_DIR}/ParallelUnitMain.cpp)
     set( TEST_MAIN_CPP "${CMAKE_CURRENT_BINARY_DIR}/ParallelUnitMain.cpp" )
     create_plato_unittester_impl( ${TEST_EXE} "${DIRECTORIES}" ${TEST_MAIN_CPP} "${TARGET_LINK_LIST}" )
     set_property(TEST ${TEST_EXE} PROPERTY PROCESSORS ${NUM_RANKS_FOR_TEST})
 
-endfunction(create_plato_parallel_unittester)
+endfunction(create_plato_parallel_integration_tester)
 
 function( create_plato_unittester_impl TEST_EXE DIRECTORIES TEST_MAIN_CPP TARGET_LINK_LIST)
 
