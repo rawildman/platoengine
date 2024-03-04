ff97bc9530a0a6aaaa05ee9073506e98fd113086
Adam Sokolow <acsokol@sandia.gov>
2024-03-04 09:09:49 -0700

    Moves common test functionality to test helper

--- functional/plato/input_parser/unittest/Test_FileList.cpp
+++ functional/plato/input_parser/unittest/Test_FileList.cpp
@@ -60,42 +60,15 @@ TEST(FileList, Insert)
     EXPECT_EQ(tFileList.mList.at(2), "b");
 }
 
-namespace
-{
-template <typename T>
-void copy_test<T>(T aFileList)
-{
-    std::vector<std::string> tCopy;
-    std::copy(aFileList.begin(), aFileList.end(), std::back_inserter(tCopy));
-    ASSERT_EQ(tCopy.size(), 4);
-    EXPECT_EQ(aFileList.mList, tCopy);
-}
-}  // namespace
-
 TEST(FileList, Iterators)
 {
     FileList tFileList{{"r", "a", "w", "r"}};
     copy_test<FileList>(tFileList);
-    /*std::vector<std::string> tCopy;
-    std::copy(tFileList.begin(), tFileList.end(), std::back_inserter(tCopy));
-    ASSERT_EQ(tCopy.size(), 4);
-    EXPECT_EQ(tFileList.mList.at(0), "r");
-    EXPECT_EQ(tFileList.mList.at(1), "a");
-    EXPECT_EQ(tFileList.mList.at(2), "w");
-    EXPECT_EQ(tFileList.mList.at(3), "r");*/
 }
 
 TEST(FileList, ConstIterators)
 {
     FileList tFileList{{"r", "a", "w", "r"}};
     copy_test<const FileList>(tFileList);
-    /*const FileList tFileList{{"r", "a", "w", "r"}};
-    std::vector<std::string> tCopy;
-    std::copy(tFileList.begin(), tFileList.end(), std::back_inserter(tCopy));
-    ASSERT_EQ(tCopy.size(), 4);
-    EXPECT_EQ(tFileList.mList.at(0), "r");
-    EXPECT_EQ(tFileList.mList.at(1), "a");
-    EXPECT_EQ(tFileList.mList.at(2), "w");
-    EXPECT_EQ(tFileList.mList.at(3), "r");*/
 }
 }  // namespace plato::input_parser::unittest
