d4b42f138780b8c95727500e6e810637d081df64
Adam Sokolow <acsokol@sandia.gov>
2024-03-06 06:24:55 -0700

    Fixes copy and paste errors in rol integration objective tests

--- functional/plato/rol_integration/unittest/Test_ROLObjectiveHimmelblau.cpp
+++ functional/plato/rol_integration/unittest/Test_ROLObjectiveHimmelblau.cpp
@@ -6,40 +6,27 @@
 #include "plato/core/Aggregate.hpp"
 #include "plato/rol_integration/ROLObjectiveFunction.hpp"
 #include "plato/rol_integration/unittest/DynamicVectorHimmelblauFunction.hpp"
+#include "plato/rol_integration/unittest/HimmelblauHelpers.hpp"
 #include "plato/test_utilities/Himmelblau.hpp"
 
 namespace plato::rol_integration::unittest
 {
 TEST(ROLObjectiveFunction, HimmelblauObjectiveValueAndGradient)
 {
-    namespace pft = plato::test_utilities;
-
-    auto tObjective = ROLObjectiveFunction{make_himmelblau_dynamic_vector_function(pft::Himmelblau{})};
-    const auto tControl = ROL::StdVector<double>{1.0, 1.0};
-    double tTolerance;
-    EXPECT_EQ(tObjective.value(tControl, tTolerance), 0.0);
-
-    auto tGradient = ROL::StdVector<double>{0.0, 0.0};
-    tObjective.gradient(tGradient, tControl, tTolerance);
-    EXPECT_EQ(tGradient[0], 0.0);
-    EXPECT_EQ(tGradient[1], 0.0);
+    const auto tArgument = make_himmelblau_dynamic_vector_function(plato::test_utilities::Himmelblau{});
+    run_himmelblau_objective_test(tArgument);
 }
 
 TEST(ROLObjectiveFunction, AggregateTwoHimmelblauObjectives)
 {
-    namespace pft = plato::test_utilities;
+    namespace pt = plato::test_utilities;
+    using HimmelblauF = std::decay_t<decltype(make_himmelblau_dynamic_vector_function(pt::Himmelblau{}))>;
+    using HimmelblauFunctionAndWeight = std::vector<std::pair<HimmelblauF, double>>;
+    const HimmelblauFunctionAndWeight tFunctionAndWeight{
+        std::make_pair(make_himmelblau_dynamic_vector_function(pt::Himmelblau{}), .30),
+        std::make_pair(make_himmelblau_dynamic_vector_function(pt::Himmelblau{}), .70)};
 
-    auto tHimmelblauFunction = make_himmelblau_dynamic_vector_function(pft::Himmelblau{});
-    using HimmelblauF = std::decay_t<decltype(tHimmelblauFunction)>;
-    auto tObjective = ROLObjectiveFunction{core::make_aggregate_function(
-        std::vector<std::pair<HimmelblauF, double>>{std::make_pair(tHimmelblauFunction, 1.0)})};
-    const auto tControl = ROL::StdVector<double>{1.0, 1.0};
-    double tTolerance;
-    EXPECT_EQ(tObjective.value(tControl, tTolerance), 0.0);
-
-    auto tGradient = ROL::StdVector<double>{0.0, 0.0};
-    tObjective.gradient(tGradient, tControl, tTolerance);
-    EXPECT_EQ(tGradient[0], 0.0);
-    EXPECT_EQ(tGradient[1], 0.0);
+    const auto tArgument = core::make_aggregate_function(tFunctionAndWeight);
+    run_himmelblau_objective_test(tArgument);
 }
 }  // namespace plato::rol_integration::unittest
