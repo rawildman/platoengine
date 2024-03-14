a7ba9d512b8b4359047c560f43b6457430120633
Adam Sokolow <acsokol@sandia.gov>
2024-03-14 13:17:33 -0600

    Fixes minor lexigraphical errors that interfere with copy paste detection

--- functional/plato/utilities/SharedLibraryUtilities.cpp
+++ functional/plato/utilities/SharedLibraryUtilities.cpp
@@ -4,7 +4,7 @@ namespace plato::utilities
 {
 void* load_shared_library(const std::filesystem::path& aSharedLibPath)
 {
-    void* const tSharedLibInterface = dlopen(aSharedLibPath.c_str(), RTLD_LAZY | RTLD_DEEPBIND);
+    void* const tSharedLibInterface = dlopen(aSharedLibPath.c_str(), RTLD_LAZY );//| RTLD_DEEPBIND);
     if (tSharedLibInterface == nullptr)
     {
         char* const tErrorMessage = dlerror();
