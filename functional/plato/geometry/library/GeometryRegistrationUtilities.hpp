39ab14b2820798af6615d76b795983e340706de5
Ray Wildman <rawildm@sandia.gov>
2024-03-21 09:40:49 -0600

    Moves and generalizes GeometryRegistrationUtilities to core

--- functional/plato/geometry/library/GeometryRegistrationUtilities.hpp
+++ functional/plato/geometry/library/GeometryRegistrationUtilities.hpp
@@ -13,50 +13,11 @@
 
 namespace plato::geometry::library::detail
 {
-/// @brief A type trait specifying if a type @a T is an alternative of variant @a VariantT
-template <typename T, typename VariantT>
-constexpr bool kIsVariantMember = false;
-
-template <typename T, typename... Ts>
-constexpr bool kIsVariantMember<T, std::variant<Ts...>> = std::disjunction_v<std::is_same<T, Ts>...>;
-
-template <typename T, typename = void>
-[[nodiscard]] std::optional<library::GeometryInput> make_variant_if_geometry(const T&)
-{
-    return std::nullopt;
-}
-
-/// @return If @a aT is an alternative of variant GeometryInput, and is non-empty, returns an optional-wrapped copy of
-/// @a aT.
-template <typename T, typename = std::enable_if_t<kIsVariantMember<T, library::GeometryInput>>>
-[[nodiscard]] std::optional<library::GeometryInput> make_variant_if_geometry(const boost::optional<T>& aT)
-{
-    if (aT)
-    {
-        return std::make_optional(library::GeometryInput{aT.value()});
-    }
-    return std::nullopt;
-}
-
-void emplace_back_if_has_value(std::vector<library::GeometryInput>& aGeometryInput,
-                               std::optional<library::GeometryInput>&& aOptionalGeometry);
-
-template <std::size_t... Is>
-[[nodiscard]] std::vector<library::GeometryInput> geometry_blocks_impl(const input_parser::ParsedInput& aInput,
-                                                                       std::integer_sequence<std::size_t, Is...>)
-{
-    std::vector<library::GeometryInput> tGeometryInput;
-    (emplace_back_if_has_value(tGeometryInput, make_variant_if_geometry(boost::fusion::at_c<Is>(aInput))), ...);
-    return tGeometryInput;
-}
 [[nodiscard]] std::vector<library::GeometryInput> geometry_blocks(const input_parser::ParsedInput& aInput);
 
 /// @return An optional GeometryInput variant, which is the first non-empty geometry input block found in @a aInput.
 ///  If no geometry block was found, an empty optional is returned.
 [[nodiscard]] std::optional<library::GeometryInput> first_geometry_block(const input_parser::ParsedInput& aInput);
-
-/// @return The name of the geometry input held by the variant @a aInput
-[[nodiscard]] std::string block_name(const library::GeometryInput& aInput);
 
 /// @return The name of the geometry input held by the variant @a aInput
 [[nodiscard]] std::string block_name(const library::ValidatedGeometryInput& aInput);
--- functional/plato/geometry/library/unittest/Test_GeometryRegistrationUtilities.cpp
+++ functional/plato/geometry/library/unittest/Test_GeometryRegistrationUtilities.cpp
@@ -4,45 +4,6 @@
 
 namespace plato::geometry::library::unittest
 {
-TEST(GeometryRegistrationUtilities, IsVariant)
-{
-    namespace pfgld = plato::geometry::library::detail;
-
-    using TestVariant = std::variant<double, char, bool>;
-    constexpr bool tDoubleInVariant = pfgld::kIsVariantMember<double, TestVariant>;
-    EXPECT_TRUE(tDoubleInVariant);
-
-    constexpr bool tIntInVariant = pfgld::kIsVariantMember<int, TestVariant>;
-    EXPECT_FALSE(tIntInVariant);
-}
-
-TEST(GeometryRegistrationUtilities, MakeVariantWrongType)
-{
-    namespace pfgl = plato::geometry::library;
-    namespace pfgld = plato::geometry::library::detail;
-    const std::optional<pfgl::GeometryInput> tResult = pfgld::make_variant_if_geometry(42);
-    EXPECT_FALSE(tResult.has_value());
-}
-
-TEST(GeometryRegistrationUtilities, MakeVariantRightTypeEmpty)
-{
-    namespace pfgl = plato::geometry::library;
-    namespace pfgld = plato::geometry::library::detail;
-    using ValidOptional = boost::optional<input_parser::density_topology>;
-    const std::optional<pfgl::GeometryInput> tResult = pfgld::make_variant_if_geometry(ValidOptional{boost::none});
-    EXPECT_FALSE(tResult.has_value());
-}
-
-TEST(GeometryRegistrationUtilities, MakeVariantRightTypeNotEmpty)
-{
-    namespace pfgl = plato::geometry::library;
-    namespace pfgld = plato::geometry::library::detail;
-    using ValidOptional = boost::optional<input_parser::density_topology>;
-    const std::optional<pfgl::GeometryInput> tResult =
-        pfgld::make_variant_if_geometry(ValidOptional{input_parser::density_topology{}});
-    EXPECT_TRUE(tResult.has_value());
-}
-
 TEST(GeometryRegistrationUtilities, GeometryBlockAllEmpty)
 {
     namespace pfgl = plato::geometry::library;
@@ -68,19 +29,6 @@ TEST(GeometryRegistrationUtilities, GeometryInputHasDensityTopology)
     auto tInput = input_parser::ParsedInput{};
     tInput.mDensityTopology.emplace();
     EXPECT_NO_THROW(auto tGeometryInput = pfgld::first_geometry_block(tInput));
-}
-
-TEST(GeometryRegistrationUtilities, BlockName)
-{
-    namespace pfgl = plato::geometry::library;
-    {
-        const auto tGeometryInput = pfgl::GeometryInput{input_parser::density_topology{}};
-        EXPECT_EQ(pfgl::detail::block_name(tGeometryInput), "density_topology");
-    }  // namespace plato::geometry::library;
-    {
-        const auto tGeometryInput = pfgl::GeometryInput{input_parser::brick_shape_geometry{}};
-        EXPECT_EQ(pfgl::detail::block_name(tGeometryInput), "brick_shape_geometry");
-    }
 }
 
 TEST(GeometryRegistrationUtilities, GeometryBlocksVector)
