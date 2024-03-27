832bb1ffe954725df52e7fe9e393b9b39b01ae90
Ray Wildman <rawildm@sandia.gov>
2024-03-27 15:45:46 -0600

    Adds function to order a map associated with a run stage key

---
+++ functional/plato/process_manager/library/StageOrdering.hpp
@@ -0,0 +1,34 @@
+#ifndef PLATO_PROCESSMANAGER_LIBRARY_STAGEORDERING
+#define PLATO_PROCESSMANAGER_LIBRARY_STAGEORDERING
+
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <vector>
+
+namespace plato::process_manager::library
+{
+enum class RunStage
+{
+    kValidate,
+    kExecute,
+    kPostProcess
+};
+
+/// @brief Converts @a aStageMap to a vector so that each item in the map is in order in the vector
+/// according to its RunStage. The order within stages is not guaranteed, only that all, e.g.,
+/// RunStage::kValidate items are before all RunStage::kExecute items.
+template <typename T>
+[[nodiscard]] std::vector<T> to_stage_ordered_vector(std::multimap<RunStage, T> aStageMap);
+
+template <typename T>
+std::vector<T> to_stage_ordered_vector(std::multimap<RunStage, T> aStageMap)
+{
+    std::vector<T> tResult;
+    std::transform(aStageMap.begin(), aStageMap.end(), std::back_inserter(tResult),
+                   [](auto& tStageAndItem) { return std::move(tStageAndItem.second); });
+    return tResult;
+}
+}  // namespace plato::process_manager::library
+
+#endif
