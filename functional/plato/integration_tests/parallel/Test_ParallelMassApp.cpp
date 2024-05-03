9c07fdb5b75fdd797dea727b33ebd72e06af0c06
Ray Wildman <rawildm@sandia.gov>
2024-05-03 10:07:06 -0600

    Swithes to use STKCommandGenerator

--- functional/plato/integration_tests/parallel/Test_ParallelMassApp.cpp
+++ functional/plato/integration_tests/parallel/Test_ParallelMassApp.cpp
@@ -6,6 +6,7 @@
 
 #include "plato/core/MeshProxy.hpp"
 #include "plato/criteria/extension/SharedLibCriterion.hpp"
+#include "plato/utilities/STKCommandGenerator.hpp"
 #include "plato/utilities/STKUtilities.hpp"
 #include "plato/utilities/StringUtilities.hpp"
 
@@ -15,7 +16,8 @@ namespace
 {
 constexpr auto kLibPath = std::string_view{"libPlatoTestMassObjective.so"};
 constexpr auto kMeshName = std::string_view{"massTest.exo"};
-constexpr auto kMeshCommand = std::string_view{"generated:1x1x1|bbox:-1,-1,-1,1,1,1"};
+const auto kMeshCommand = utilities::STKCommandGenerator{
+    /*.mElements=*/{1u, 1u, 1u}, /*.mLowerBounds=*/{-1.0, -1.0, -1.0}, /*.mUpperBounds=*/{1.0, 1.0, 1.0}};
 }  // namespace
 
 TEST(ParallelMassObjective, CallValue)
@@ -26,9 +28,9 @@ TEST(ParallelMassObjective, CallValue)
     const auto tSharedLib = criteria::extension::SharedLibCriterion{std::string{kLibPath}, {}, tComm};
 
     const auto tRankMeshName = utilities::concatenate(kMeshName, '.', tComm.rank());
-    utilities::write_mesh(tRankMeshName, utilities::create_mesh(kMeshCommand));
+    utilities::write_mesh(tRankMeshName, utilities::create_mesh(kMeshCommand.toString()));
     const double tMass = tSharedLib.f(core::MeshProxy{tRankMeshName, {}});
-    EXPECT_DOUBLE_EQ(tMass, 8.0);
+    EXPECT_DOUBLE_EQ(tMass, kMeshCommand.volume());
 
     std::filesystem::remove(tRankMeshName);
 }
@@ -41,7 +43,7 @@ TEST(ParallelMassObjective, CallGradient)
     const auto tSharedLib = criteria::extension::SharedLibCriterion{std::string{kLibPath}, {}, tComm};
 
     const auto tRankMeshName = utilities::concatenate(kMeshName, '.', tComm.rank());
-    utilities::write_mesh(tRankMeshName, utilities::create_mesh(kMeshCommand));
+    utilities::write_mesh(tRankMeshName, utilities::create_mesh(kMeshCommand.toString()));
     const auto tGrad = tSharedLib.df(core::MeshProxy{tRankMeshName, {}});
 
     const std::vector<double> tGold(24, 0.0);
