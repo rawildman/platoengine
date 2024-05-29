08a685ef6ef985ee65f193d7ddb38603891d517a
Adam Sokolow <acsokol@sandia.gov>
2024-05-29 13:52:13 -0600

    Used ncdump to get exo -> cdf text version, and then revised our scripts to use ncgen to go from cdf text -> cdf. Modified unit tests to use cdf which they can open.

--- functional/plato/cmake/copy_test_utility_data_files_and_convert_to_cdf.cmake
+++ functional/plato/cmake/copy_test_utility_data_files_and_convert_to_cdf.cmake
@@ -1,8 +1,8 @@
-# copy_test_utility_data_files_and_convert_to_exodus
+# copy_test_utility_data_files_and_convert_to_cdf
 #   Copies all the files from ${FUNCTIONAL_TEST_DATA_DIR} to the build and install paths.
 #   Runs txtexo on each file to convert them from git friendly ascii txt files of the meshes to binary exo. 
 #   Creates two variables to refer to in the code FUNCTIONAL_TEST_DATA_BUILD_PATH and FUNCTIONAL_TEST_DATA_INSTALL_PATH.
-macro( copy_test_utility_data_files_and_convert_to_exodus )
+macro( copy_test_utility_data_files_and_convert_to_cdf )
 
     set( FUNCTIONAL_TEST_DATA_BUILD_PATH "${CMAKE_CURRENT_BINARY_DIR}/test_data")
     set( FUNCTIONAL_TEST_DATA_INSTALL_PATH "${CMAKE_INSTALL_PREFIX}/test_data")
@@ -15,11 +15,11 @@ macro( copy_test_utility_data_files_and_convert_to_exodus )
         
         get_filename_component(BASE_FILE_NAME ${TXTFILE} NAME_WLE)
         set(FULLPATH_TXT_FILE "${FUNCTIONAL_TEST_DATA_DIR}/${BASE_FILE_NAME}.txt")
-        set(BUILDPATH_EXO_FILE "${FUNCTIONAL_TEST_DATA_BUILD_PATH}/${BASE_FILE_NAME}.exo")
-        set(INSTALLPATH_EXO_FILE "${FUNCTIONAL_TEST_DATA_INSTALL_PATH}/${BASE_FILE_NAME}.exo")
-        execute_process(COMMAND "txtexo" ${FULLPATH_TXT_FILE} ${BUILDPATH_EXO_FILE} ) 
-        execute_process(COMMAND "txtexo" ${FULLPATH_TXT_FILE} ${INSTALLPATH_EXO_FILE} ) 
+        set(BUILDPATH_EXO_FILE "${FUNCTIONAL_TEST_DATA_BUILD_PATH}/${BASE_FILE_NAME}.cdf")
+        set(INSTALLPATH_EXO_FILE "${FUNCTIONAL_TEST_DATA_INSTALL_PATH}/${BASE_FILE_NAME}.cdf")
+        execute_process(COMMAND "ncgen" ${FULLPATH_TXT_FILE} -o ${BUILDPATH_EXO_FILE} ) 
+        execute_process(COMMAND "ncgen" ${FULLPATH_TXT_FILE} -o ${INSTALLPATH_EXO_FILE} ) 
         
     endforeach(TXTFILE)
 
-endmacro(copy_test_utility_data_files_and_convert_to_exodus)
+endmacro(copy_test_utility_data_files_and_convert_to_cdf)
